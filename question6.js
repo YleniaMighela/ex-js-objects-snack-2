af04f1f1c9ed89db24d7559997039d298c147e76
Ylenia <yleniamighela@gmail.com>
2025-04-24 11:58:29 +0200

    Code Question 6 (Bonus)

---
+++ question6.js
@@ -0,0 +1,23 @@
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            showAddress: () => {
+                console.log("Main Street 123");
+            }
+        },
+        isOpen: true,
+    }
+}
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché? mi verrebbe da dire lo spread operator, ma ci sono degli oggetti annidati, allora ho pensato lo structured ma non prende le funzioni.....
